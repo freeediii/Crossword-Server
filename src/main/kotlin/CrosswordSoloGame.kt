52f5d0203f90aefac552a329ad8b82060bd2861b
Freeedyboy <frimarom@yahoo.de>
2023-02-16 13:12:24 +0100

    adjusted the first message for a solo game

--- src/main/kotlin/CrosswordSoloGame.kt
+++ src/main/kotlin/CrosswordSoloGame.kt
@@ -2,6 +2,6 @@ class CrosswordSoloGame(val player: OnlinePlayer,
                         size: Int): CrosswordGame(size) {
 
     init {
-        this.player.sendMessage("messageType: GAME, gameType: SOLO, questions: {"+ this.questionPool.joinToString(",")+"}, questionRight: -1")
+        this.player.sendMessage("{\"messageType\": \"GAME\", \"gameType\": \"SOLO\", \"questions\": {\"${this.questionPool.joinToString("\", \"")}\"}}")
     }
 }
